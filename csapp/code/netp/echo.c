7189806542a03a433b79cacc2a70608ee65df740
LAPTOP-0TGLVV15\j2 <jhd124@hotmail.com>
2019-12-29 10:37:46 +0800

    network program

--- csapp/code/netp/echo.c
+++ csapp/code/netp/echo.c
@@ -2,7 +2,7 @@
  * echo - read and echo text lines until client closes connection
  */
 /* $begin echo */
-#include "csapp.h"
+#include "../include/csapp.h"
 
 void echo(int connfd) 
 {
@@ -12,7 +12,7 @@ void echo(int connfd)
 
     Rio_readinitb(&rio, connfd);
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
-	printf("server received %d bytes\n", n);
+	printf("server received %lu bytes\n", n);
 	Rio_writen(connfd, buf, n);
     }
 }
